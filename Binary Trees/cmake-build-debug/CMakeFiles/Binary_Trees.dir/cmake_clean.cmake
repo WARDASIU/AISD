579b4be5c5a7b75e1f4fdd1dd7f8eaf9ee4ed05a
WARDASIU <72788607+WARDASIU@users.noreply.github.com>
2021-01-27 19:30:34 +0100

    Add files via upload

---
+++ Binary Trees/cmake-build-debug/CMakeFiles/Binary_Trees.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Binary_Trees.exe"
+  "Binary_Trees.pdb"
+  "CMakeFiles/Binary_Trees.dir/main.cpp.o"
+  "libBinary_Trees.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Binary_Trees.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
